09e9bafd0603c3f9a6db6ebfced3c1752f405810
kiwi <davincidx0418@gmail.com>
2022-03-10 10:50:50 +0800

    save username to Preferences

--- app/src/main/java/com/kiwi/atm/FirstFragment.kt
+++ app/src/main/java/com/kiwi/atm/FirstFragment.kt
@@ -1,10 +1,12 @@
 package com.kiwi.atm
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import com.kiwi.atm.databinding.FragmentFirstBinding
 
@@ -31,9 +33,33 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        //若user先前有打過資料，則可記住user的資料且直接替user輸入
+        val pref= requireContext().getSharedPreferences("atm",Context.MODE_PRIVATE)
+        val prefUser = pref.getString("USER", "")
+        if(prefUser != ""){
+            binding.edUsername.setText(prefUser)
+        }
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            //Login stuff
+            val username = binding.edUsername.text.toString()
+            val password = binding.edPassword.text.toString()
+            if( username == "jack" && password == "1234"){
+                //save username to preference
+                //val pref = requireContext().getSharedPreferences("atm",Context.MODE_PRIVATE)
+                pref.edit()
+                    .putString("USER", username)
+                    .putInt("LEVEL",3)
+                    //apply有空就寫  commit馬上就寫
+                    .apply()//.commit()
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }else{
+                //error
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Login")
+                    .setMessage("Login Failed")
+                    .setPositiveButton("OK",null)
+                    .show()
+            }
         }
     }
 
